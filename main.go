66f1a7d3d862a45a371f1343351e207e1b3016cd
BetterYifan <1197688675@qq.com>
2022-07-05 22:59:22 +0800

    update

--- main.go
+++ main.go
@@ -14,8 +14,6 @@ func main() {
 	http.HandleFunc("/", sayHello)
 
 	//2.设置监听的TCP地址并启动服务
-	//参数1：TCP地址(IP+Port)
-	//参数2：当设置为nil时表示使用DefaultServeMux
-	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	log.Fatal(err)
 }
